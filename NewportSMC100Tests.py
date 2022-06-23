959f757111327bcd6843fee91a9ca1359b29b5ed
Benjamin Saggin <benjamin.saggin@live.fr>
2022-06-23 11:33:35 +0200

    [Add] Improved scan procedure in tests

--- NewportSMC100Tests.py
+++ NewportSMC100Tests.py
@@ -1,12 +1,21 @@
 from NewportSMC100 import MainController
+import numpy
 
 mainController = MainController('com3')
 xController = mainController.NewController()
 yController = mainController.NewController(2)
 zController = mainController.NewController(3)
-xController.GoTo(xController.MaxPosition, wait=False)
-zController.GoTo(zController.MaxPosition, wait=False)
-yController.GoTo(yController.MaxPosition, wait=True)
-xController.GoTo(0, wait=False)
-zController.GoTo(0, wait=False)
-yController.GoTo(0, wait=True)
+y = yController.Position
+yPositions = numpy.arange(yController.MinPosition, yController.MaxPosition, 1)
+z = zController.Position
+zPositions = numpy.arange(zController.MinPosition, zController.MaxPosition, 1)
+for x in numpy.arange(xController.MinPosition, xController.MaxPosition, 1):
+    xController.GoTo(x, wait=True)
+
+    isYCloserToMin = abs(y - yController.MinPosition) < abs(y - yController.MaxPosition)
+    for y in (yPositions if isYCloserToMin else yPositions[::-1]):
+        yController.GoTo(y, wait=True)
+
+        isZCloserToMin = abs(z - zController.MinPosition) < abs(z - zController.MaxPosition)
+        for z in (zPositions if isZCloserToMin else zPositions[::-1]):
+            zController.GoTo(z, wait=True)
