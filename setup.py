f769dda8fb41199e95c51cc5b024601da02f12e0
Benjamin Saggin <benjamin.saggin@live.fr>
2022-11-21 18:53:33 +0100

    [Add] Package generation

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+install_requires = [line.strip() for line in open("requirements.txt").readlines()]
+
+setuptools.setup(
+    name='pyNewportController',
+    version='0.0.1',
+    author='Benjamin SAGGIN',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Ben3094/pyNewportController',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Ben3094/pyNewportController/issues"
+    },
+    license='MIT',
+    packages=['pyNewportController'],
+    install_requires=install_requires,
+)
